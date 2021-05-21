c8b716951a036ce06cb72234eae65aa9f0fb792b
user <user@DESKTOP-T3DOGRQ>
2021-05-21 13:08:07 +0900

    ok

---
+++ JavaWorkShop15/src/main/java/com/My/JavaWorkShop15/App.java
@@ -0,0 +1,13 @@
+package com.My.JavaWorkShop15;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ JavaWorkShop15/src/main/java/com/test/Sample01.java
@@ -0,0 +1,9 @@
+package com.test;
+
+public class Sample01 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
